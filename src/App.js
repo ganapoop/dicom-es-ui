a695d442d10b4bcd7ab82bcb15735b694c4f4e7a
rksquared <rahul@rahulkrish.com>
2018-04-18 11:47:54 -0400

    basic table setup complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Axios from 'axios';
+import { Button, Input, Table, Icon, Divider } from 'antd';
 
-import { Button } from 'antd';
+const Search = Input.Search;
+
+const columns = [{
+  title: 'SOPInstanceUID',
+  dataIndex: 'sopInstanceUID',
+  key: 'sopUID',
+  render: text => <a href="javascript:;">{text}</a>,
+}, {
+  title: 'PatientID',
+  dataIndex: 'patientId',
+  key: 'pID',
+}, {
+  title: 'Study Date',
+  dataIndex: 'studyDate',
+  key: 'address',
+}, {
+  title: 'Action',
+  key: 'action',
+  render: (text, record) => (
+    <span>
+      <a href="javascript:;">Action 一 {record.sopUID}</a>
+      <Divider type="vertical" />
+      <a href="javascript:;">Delete</a>
+      <Divider type="vertical" />
+      <a href="javascript:;" className="ant-dropdown-link">
+        More actions <Icon type="down" />
+      </a>
+    </span>
+  ),
+}];
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      query: '',
+      dicom: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.getSearchResults = this.getSearchResults.bind(this);
+  }
+
+  componentDidMount() {
+    this.getSearchResults('PT');
+  }
+
+  handleChange(e) {
+    let { value } = e.target
+    
+    this.setState({
+      query: value
+    });
+  }
+
+  getSearchResults(query) {
+    Axios.get(`http://localhost:5005/search?q=${query}`)
+      .then(({data}) => {
+        console.log(`return payload from server after query submission: ${JSON.stringify(data)}, ${Array.isArray(data)}`);
+        this.setState({
+          dicom: data
+        });
+      })
+      .catch((err) => console.error(`ERROR when querying server: ${err}`));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <Button type="primary">Test</Button>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Search
+          placeholder="input search text"
+          value={this.state.query}
+          onSearch={() => this.getSearchResults(this.state.query)}
+          onChange={this.handleChange}
+          enterButton
+          style={{ width: 600 }}
+        />
+        <pre>DICOM METADATA in STATE: {JSON.stringify(this.state.dicom)}</pre>
+        <Table columns={columns} dataSource={this.state.dicom} />
       </div>
     );
   }
