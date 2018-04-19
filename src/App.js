1174ffc41a06543ec68eca4220b1e57858311c84
rksquared <rahul@rahulkrish.com>
2018-04-19 00:57:23 -0400

    fixed pagination

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-import { Layout, Button, Input, Table, Icon, Divider, Avatar } from 'antd';
+import { Layout, Button, Input, Table, Icon, Divider, Avatar} from 'antd';
 import logo from './logo.png'
 
 const Search = Input.Search;
